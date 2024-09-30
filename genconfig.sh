f332b8345e6430433b2b310a0cdb89c077813ddd
yifangd <yifangd0@gmail.com>
2024-09-30 16:58:21 -0500

    enable ImageBuilder
    Signed-off-by: yifangd <yifangd0@gmail.com>

--- genconfig.sh
+++ genconfig.sh
@@ -5,7 +5,7 @@ wget https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/config.b
 cat config.buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
 echo CONFIG_TARGET_ALL_PROFILES=n >> .config
 echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
-echo CONFIG_IB=n >> .config
+echo CONFIG_IB=y >> .config
 echo CONFIG_TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
 echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
 echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300=\"\" >> .config
